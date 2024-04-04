17f4bcb1571682de15f0566ce9c18eae6df84d2c
wlindor <47373982+wlindor@users.noreply.github.com>
2024-04-04 14:07:34 -0500

    updating

--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,11 @@
 from flask import Flask
-# SQLAlchemy is a library that allows you to interact with a SQL database
 from flask_sqlalchemy import SQLAlchemy
-# Cross origin requests allows you to make a request to this backend from a different domain
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-# now we can send requests to this backend from a different domain
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydatabase.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
--- backend/main.py
+++ backend/main.py
@@ -2,65 +2,68 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
-# Get decorator is used to tell the app which URL should trigger the function
-@app.route('/contacts', methods=["GET"])
+
+@app.route("/contacts", methods=["GET"])
 def get_contacts():
     contacts = Contact.query.all()
-    contacts_json = [contact.to_json() for contact in contacts]
-    return jsonify({"contacts": contacts_json})
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
 
-@app.route('/create_contact', methods=["POST"])
+
+@app.route("/create_contact", methods=["POST"])
 def create_contact():
-    first_name = request.json.get('firstName')
-    last_name = request.json.get('lastName')
-    email = request.json.get('email')
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
 
     if not first_name or not last_name or not email:
-        return(jsonify({"error": "You must include a first name, last name and email"}), 400)
-    
-    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}),
+            400,
+        )
 
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
         db.session.add(new_contact)
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
-    finally:
-        db.session.close()
 
-    return jsonify({"message": "Contact created successfully"}), 201
+    return jsonify({"message": "User created!"}), 201
 
-@app.route('/update_contact/<int:id>', methods=["PATCH"])
+
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
 def update_contact(user_id):
     contact = Contact.query.get(user_id)
 
     if not contact:
-        return jsonify({"error": "Contact not found"}), 404
+        return jsonify({"message": "User not found"}), 404
 
     data = request.json
-    contact.first_name = data.get('firstName', contact.first_name)
-    contact.last_name = data.get('lastName', contact.last_name)
-    contact.email = data.get('email', contact.email)
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
 
     db.session.commit()
 
-    return jsonify({"message": "Contact updated successfully"}), 201
+    return jsonify({"message": "Usr updated."}), 200
 
-@app.route('/delete_contact/<int:id>', methods=["DELETE"])
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
 
     if not contact:
-        return jsonify({"error": "Contact not found"}), 404
+        return jsonify({"message": "User not found"}), 404
 
     db.session.delete(contact)
     db.session.commit()
 
-    return jsonify({"message": "Contact deleted successfully"}), 201
+    return jsonify({"message": "User deleted!"}), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     with app.app_context():
-        # create_all() will create any tables in the database that do not already exist
         db.create_all()
 
     app.run(debug=True)
